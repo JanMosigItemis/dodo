d5efa0cec686e5a79ba9fa0122aed4f2e2dd06ed
Jan Mosig <jan.mosig@itemis.de>
2019-01-16 16:50:58 +0100

    Reconfigured email notifications

--- src/test/java/de/itemis/jmo/dodo/JUnit5ExampleTest.java
+++ src/test/java/de/itemis/jmo/dodo/JUnit5ExampleTest.java
@@ -1,7 +1,5 @@
 package de.itemis.jmo.dodo;
 
-import static org.junit.Assert.fail;
-
 import org.junit.jupiter.api.Test;
 
 public class JUnit5ExampleTest {
@@ -9,10 +7,5 @@ public class JUnit5ExampleTest {
     @Test
     public void justAnExample() {
         System.out.println("Hello World!");
-    }
-
-    @Test
-    public void thisTestFails() {
-        fail("Failing intentionally.");
     }
 }
